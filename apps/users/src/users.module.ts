1d25559d483bfaa0d8ec07171cc9c8ecce339197
Dimitar Sashev Dimitrov <dimitar.sashev@gmail.com>
2024-07-11 16:25:46 +0300

    update

--- apps/users/src/users.module.ts
+++ apps/users/src/users.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { UsersModule } from './users/users.module';
+import { UsersResolver } from './users.resolver';
+import { GraphQLModule } from '@nestjs/graphql';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [UsersController],
-  providers: [UsersService],
+  imports: [
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: {
+        federation: 2,
+      },
+    }),
+  ],
+  providers: [UsersResolver, UsersService],
 })
 export class UsersModule {}
